0b8901b1d6df2d5b6054094c300bac8cf51fa625
Jason Allen <jallend1@gmail.com>
2023-06-04 12:07:49 -0700

    Makes header text on blog page larger

--- wp-content/themes/local1857/patterns/blog-header.php
+++ wp-content/themes/local1857/patterns/blog-header.php
@@ -7,9 +7,10 @@
  */
 ?>
 <!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri('assets/images/people-sitting.jpg')); ?>","dimRatio":50,"minHeight":259,"minHeightUnit":"px","contentPosition":"bottom center","layout":{"type":"default"}} -->
-<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:259px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri('assets/images/people-sitting.jpg')); ?>" data-object-fit="cover" />
-    <div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","fontSize":"large"} -->
-        <h2 class="wp-block-heading has-text-align-center has-large-font-size">KCLS Voice News</h2>
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:259px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/people-sitting.jpg')); ?>" data-object-fit="cover" />
+    <div class="wp-block-cover__inner-container">
+        <!-- wp:heading {"fontSize":"x-large"} -->
+        <h2 class="wp-block-heading has-x-large-font-size">KCLS Voice News</h2>
         <!-- /wp:heading -->
     </div>
 </div>

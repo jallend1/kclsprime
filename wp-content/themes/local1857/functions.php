e523a7b3532b4e29892d6600e29802a95963f60b
Jason Allen <jallend1@gmail.com>
2023-06-06 17:47:24 -0700

    Replaces [...] with ... and adds a Read More link to the sidebar on blog posts

--- wp-content/themes/local1857/functions.php
+++ wp-content/themes/local1857/functions.php
@@ -5,10 +5,14 @@ function local_1857_styles()
     wp_enqueue_style('local_1857_styles', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'));
 }
 
+add_action('wp_enqueue_scripts', 'local_1857_styles');
+
 function local_1857_editor_styles()
 {
     add_editor_style('editor-style.css');
 }
+
+add_action('after_setup_theme', 'local_1857_editor_styles');
 
 // If the block being rendered is core/latest-posts, if the post does not have a featured image, then make local1857logo.png the default
 function local_1857_default_post_image($image, $post, $args)
@@ -22,6 +26,24 @@ function local_1857_default_post_image($image, $post, $args)
 
 add_filter('render_block_core/latest-posts', 'local_1857_default_post_image', 10, 3);
 
+function local_1857_excerpt_more($more)
+{
+    return '...';
+}
 
-add_action('wp_enqueue_scripts', 'local_1857_styles');
-add_action('after_setup_theme', 'local_1857_editor_styles');
+add_filter('excerpt_more', 'local_1857_excerpt_more');
+
+// If the post type is post, add a "Read More" link to the end of the excerpt
+function local_1857_excerpt_read_more_link($excerpt)
+{
+    if (get_post_type() === 'post') {
+        $excerpt .= '
+        <div class="local1857-read-more-link-container">
+            <a href="' . get_permalink() . '" class="local1857-read-more-link">Read More »</a>
+        </div>';
+    }
+
+    return $excerpt;
+}
+
+add_filter('get_the_excerpt', 'local_1857_excerpt_read_more_link');

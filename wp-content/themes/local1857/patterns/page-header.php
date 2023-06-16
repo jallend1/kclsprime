5b15e72d26be8329ae95bb6248e9ed1d536bdc52
Jason Allen <jallend1@gmail.com>
2023-06-15 17:12:49 -0700

    Creates page-header and updates page template

---
+++ wp-content/themes/local1857/patterns/page-header.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Title: Local 1857 Page Header
+ * Slug: local1857/page-header
+ * Categories: text, header
+ */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri('assets/images/bookshelf.jpg')); ?>","dimRatio":50,"focalPoint":{"x":0.5,"y":0.3},"minHeight":259,"minHeightUnit":"px","contentPosition":"bottom center","layout":{"type":"default"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:259px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/bookshelf.jpg')); ?>" style="object-position:50% 30%" data-object-fit="cover" data-object-position="50% 30%"/>
+    <div class="wp-block-cover__inner-container">
+        <!-- wp:post-title {"className":"local1857-page-title"} /-->
+    </div>
+</div>
+<!-- /wp:cover -->

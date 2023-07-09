cef4cca10df9459d2345e85fd6f2d60b84302e29
Jason Allen <jallend1@gmail.com>
2023-07-09 13:53:12 -0700

    Adjusts overlay in the editor so it renders properly on the backend

--- wp-content/plugins/latest-news/build/index.asset.php
+++ wp-content/plugins/latest-news/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd8706d4308eadd7df499');
+<?php return array('dependencies' => array('wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '771ed9f924bb1fad8395');

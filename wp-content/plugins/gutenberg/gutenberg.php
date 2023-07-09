03235f418b8f125250375f69f954f68c65edca9a
Jason Allen <jallend1@gmail.com>
2023-07-09 12:31:52 -0700

    Updates Gutenberg plugin

--- wp-content/plugins/gutenberg/gutenberg.php
+++ wp-content/plugins/gutenberg/gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.1
  * Requires PHP: 5.6
- * Version: 16.0.0
+ * Version: 16.1.2
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '16.0.0' );
-define( 'GUTENBERG_GIT_COMMIT', '87a4a02434d03f239d2917b2c38c8a488c2aa89c' );
+define( 'GUTENBERG_VERSION', '16.1.2' );
+define( 'GUTENBERG_GIT_COMMIT', '15bdeed0ede3de8d595492fd46451076db80f558' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();

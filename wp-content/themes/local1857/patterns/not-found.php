05d59e36f81cfe1eedd0f6bb2417e15119384fca
Jason Allen <jallend1@gmail.com>
2023-07-10 13:43:49 -0700

    Removes comment that was causing error to appear in console

--- wp-content/themes/local1857/patterns/not-found.php
+++ wp-content/themes/local1857/patterns/not-found.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Local 1857 Not Found
  * Slug: local1857/notfound
@@ -6,18 +7,20 @@
  */
 ?>
 
-<!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/card-catalog.jpg')); ?>","id":15476,"dimRatio":50,"minHeight":85,"minHeightUnit":"vh","isDark":false,"className":"local1857-404-cover","layout":{"type":"constrained"}} -->
-<div class="wp-block-cover is-light local1857-404-cover" style="min-height:85vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-15476" alt="" src="<?php echo esc_url( get_theme_file_uri('assets/images/card-catalog.jpg')); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","textColor":"kcls-voice-white","fontSize":"x-large"} -->
-    <h2 class="wp-block-heading has-text-align-center has-kcls-voice-white-color has-text-color has-x-large-font-size">Cataloging Error</h2>
-    <!-- /wp:heading -->
-    
-    <!-- wp:paragraph {"align":"center","textColor":"kcls-voice-white"} -->
-    <p class="has-text-align-center has-kcls-voice-white-color has-text-color">Sorry, but we couldn't find the page you were looking for.</p>
-    <!-- /wp:paragraph -->
-    
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-    <div class="wp-block-buttons"><!-- wp:button {"width":25,"fontSize":"medium"} -->
-    <div class="wp-block-button has-custom-width wp-block-button__width-25 has-custom-font-size has-medium-font-size"><a href="<?php echo site_url(); ?>" class="wp-block-button__link wp-element-button">Return Home</a></div>
-    <!-- /wp:button --></div>
-    <!-- /wp:buttons -->
-    <!-- /wp:cover -->
+<!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri('assets/images/card-catalog.jpg')); ?>","id":15476,"dimRatio":50,"minHeight":85,"minHeightUnit":"vh","isDark":false,"className":"local1857-404-cover","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light local1857-404-cover" style="min-height:85vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-15476" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/card-catalog.jpg')); ?>" data-object-fit="cover" />
+    <div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","textColor":"kcls-voice-white","fontSize":"x-large"} -->
+        <h2 class="wp-block-heading has-text-align-center has-kcls-voice-white-color has-text-color has-x-large-font-size">Cataloging Error</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"align":"center","textColor":"kcls-voice-white"} -->
+        <p class="has-text-align-center has-kcls-voice-white-color has-text-color">Sorry, but we couldn't find the page you were looking for.</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+        <div class="wp-block-buttons"><!-- wp:button {"width":25,"fontSize":"medium"} -->
+            <div class="wp-block-button has-custom-width wp-block-button__width-25 has-custom-font-size has-medium-font-size"><a href="<?php echo site_url(); ?>" class="wp-block-button__link wp-element-button">Return Home</a></div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
+        <!-- /wp:cover -->
--- wp-content/themes/local1857/patterns/page-header.php
+++ wp-content/themes/local1857/patterns/page-header.php
@@ -8,12 +8,11 @@
 ?>
 
 <!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri('assets/images/bookshelf.jpg')); ?>","dimRatio":50,"focalPoint":{"x":0.5,"y":0.3},"minHeight":259,"minHeightUnit":"px","contentPosition":"bottom center","layout":{"type":"default"}} -->
-<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:259px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/bookshelf.jpg')); ?>" style="object-position:50% 30%" data-object-fit="cover" data-object-position="50% 30%"/>
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center" style="min-height:259px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/bookshelf.jpg')); ?>" style="object-position:50% 30%" data-object-fit="cover" data-object-position="50% 30%" />
     <div class="wp-block-cover__inner-container">
-        <!-- If the URL indicates search results, display the search terms -->
         <?php if (strpos($_SERVER['REQUEST_URI'], '/?s=') !== false) : ?>
             <!-- wp:query-title {"type":"search","textAlign":"center","level":2,"className":"local1857-page-title"} /-->
-        <!-- If it's just a regular page, display the title -->
+            <!-- If it's just a regular page, display the title -->
         <?php else : ?>
             <!-- wp:post-title {"className":"local1857-page-title"} /-->
         <?php endif; ?>

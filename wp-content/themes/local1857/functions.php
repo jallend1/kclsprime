c844832155b0d463a982234652f5be59f5337ef7
Jason Allen <jallend1@gmail.com>
2023-08-02 18:08:57 -0700

    Removes commented out vestigial code from functions

--- wp-content/themes/local1857/functions.php
+++ wp-content/themes/local1857/functions.php
@@ -14,26 +14,6 @@ function local_1857_editor_styles()
 
 add_action('after_setup_theme', 'local_1857_editor_styles');
 
-// If the block being rendered is core/latest-posts, add default image if none is set
-// function local_1857_default_post_image($image, $post, $args)
-// {
-//     if ($image) {
-//         return $image;
-//     }
-
-//     return get_template_directory_uri() . '/assets/images/local1857logo.png';
-// }
-
-// add_filter('render_block_core/latest-posts', 'local_1857_default_post_image', 10, 3);
-
-
-// function local_1857_excerpt_more($more)
-// {
-//     return '...';
-// }
-
-// add_filter('excerpt_more', 'local_1857_excerpt_more');
-
 function local_1857_default_post_thumbnail($html, $post_id, $post_thumbnail_id, $size, $attr)
 {
     if ($html) {
@@ -49,18 +29,3 @@ function local_1857_default_post_thumbnail($html, $post_id, $post_thumbnail_id, 
 }
 
 add_filter('post_thumbnail_html', 'local_1857_default_post_thumbnail', 10, 5);
-
-// If the post type is post, add a "Read More" link to the end of the excerpt
-// function local_1857_excerpt_read_more_link($excerpt)
-// {
-//     if (get_post_type() === 'post') {
-//         $excerpt .= '
-//         <div class="local1857-read-more-link-container">
-//             <a href="' . get_permalink() . '" class="local1857-read-more-link">Read More »</a>
-//         </div>';
-//     }
-
-//     return $excerpt;
-// }
-
-// add_filter('get_the_excerpt', 'local_1857_excerpt_read_more_link');

1647120334a6e3b3207563445bea5c7e74a07101
Jason Allen <jallend1@gmail.com>
2023-08-16 17:50:42 -0700

    Creates Leadership pattern and adds additional icons for executive roles

---
+++ wp-content/themes/local1857/patterns/leadership.php
@@ -0,0 +1,198 @@
+<?php
+
+/**
+ * Title: Local 1857 Leadership
+ * Slug: local1857/leadership
+ * Categories: text
+ */
+?>
+
+<!-- wp:group {"className":"local1857-section-content","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group local1857-section-content"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"className":"local1857-section-heading"} -->
+<h2 class="wp-block-heading local1857-section-heading">Our Leadership</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-menu-container","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-group local1857-menu-container"><!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Union President</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-president.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-president.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Kim Atchley</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><a href="mailto:president@kclsvoice.org">president@kclsvoice.org</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">VP / Chief Steward</h2>
+<!-- /wp:heading -->
+
+
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-vp.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-vp.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Aisha Burrell</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><a href="mailto:chiefstewardlocal1857@gmail.com">chiefstewardlocal1857@gmail.com</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Union Treasurer</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-treasurer.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-treasurer.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Victoria Kunze</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><a href="mailto:treasurer@kclsvoice.org">treasurer@kclsvoice.org</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Union Secretary</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-secretary.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-secretary.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Elaine Ang</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"}},"className":"local1857-section-heading","fontSize":"medium"} -->
+<h2 class="wp-block-heading local1857-section-heading has-medium-font-size" style="text-transform:uppercase">Members-At-Large</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-menu-container","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-group local1857-menu-container"><!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">LTA at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-lta.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-lta.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Linda Jarvis</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">LTA at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-lta.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-lta.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Christi Wheelock</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Librarian at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-librarian.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-librarian.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Jennifer Duffy</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Maintenance <br>at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-maint.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-maint.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Katie Mathews</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Page at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-page.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-page.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Marcela Mets</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">All Other </h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Classifications at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-member.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-member.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Stan Brooks</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"local1857-officer-card","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group local1857-officer-card"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">PSA at Large</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"url":"<?php echo esc_url( get_theme_file_uri('assets/images/icon-psa.png')); ?>","width":75,"height":75,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="<?php echo esc_url( get_theme_file_uri('assets/images/icon-psa.png')); ?>" alt="" width="75" height="75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">VACANT</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
